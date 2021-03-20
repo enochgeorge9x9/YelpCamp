574f38f33c1b596e4cbba4dd302892e6f1284048
enochgeorge9x9 <enochgeorge1999@gmail.com>
2021-03-20 17:00:30 +0300

    Added Basic Styles to Home Page, Edit Page and Add page. Also added controls in the Maps

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = MAPBOX_TOKEN;
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10',
+    container: 'cluster-map',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: [-103.59179687498357, 40.66995747013945],
     zoom: 3
 });
@@ -128,3 +128,5 @@ map.on('load', function () {
         map.getCanvas().style.cursor = '';
     });
 });
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
